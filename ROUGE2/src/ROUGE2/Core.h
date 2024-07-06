b37df81127d928463d4c9bb822b6ca1b236cd7a3
RanggaGunarto <ranggagunarto@gmail.com>
2024-07-06 10:51:02 +0800

    Fix Fatal Linking (Conversion to Full Static)

--- ROUGE2/src/ROUGE2/Core.h
+++ ROUGE2/src/ROUGE2/Core.h
@@ -1,23 +1,18 @@
 #pragma once
 
 #ifdef R2_PLATFORM_WINDOWS
-	#ifdef R2_BUILD_DLL
-		#define ROUGE2_API __declspec(dllexport)
-
-	#else
-		#define ROUGE2_API __declspec(dllimport)
-	#endif
+#define ROUGE2_API
 
 #else
-	#error ROUGE2 ONLY SUPPORTS x64 WINDOWS!
+#error ROUGE2 ONLY SUPPORTS x64 WINDOWS!
 #endif
 
 #ifdef R2_ENABLE_ASSERTS
-	#define R2_ASSERT(x, ...) { if(!(x)) { R2_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
-	#define R2_CORE_ASSERT(x, ...) { if(!(x)) { R2_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#define R2_ASSERT(x, ...) { if(!(x)) { R2_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#define R2_CORE_ASSERT(x, ...) { if(!(x)) { R2_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 #else
-	#define R2_ASSERT(x, ...)
-	#define R2_CORE_ASSERT(x, ...)
+#define R2_ASSERT(x, ...)
+#define R2_CORE_ASSERT(x, ...)
 #endif
 
 #define BIT(x) (1 << x)
--- premake5.lua
+++ premake5.lua
@@ -22,82 +22,98 @@ include "ROUGE2/vendor/GLFW"
 include "ROUGE2/vendor/Glad"
 include "ROUGE2/vendor/imgui"
 
+workspace "ROUGE2"
+    architecture "x64"
+    startproject "Sandbox"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+IncludeDir["GLFW"] = "ROUGE2/vendor/GLFW/include"
+IncludeDir["Glad"] = "ROUGE2/vendor/Glad/include"
+IncludeDir["ImGui"] = "ROUGE2/vendor/imgui"
+IncludeDir["glm"] = "ROUGE2/vendor/glm"
+
+include "ROUGE2/vendor/GLFW"
+include "ROUGE2/vendor/Glad"
+include "ROUGE2/vendor/imgui"
+
 project "ROUGE2"
-	location "ROUGE2"
-	kind "SharedLib"
-	language "C++"
-	cppdialect "C++17"
-	staticruntime "on"
+    location "ROUGE2"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	pchheader "r2pch.h"
-	pchsource "ROUGE2/src/r2pch.cpp"
+    pchheader "r2pch.h"
+    pchsource "ROUGE2/src/r2pch.cpp"
 
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp",
-		"%{prj.name}/vendor/glm/glm/**.hpp",
-		"%{prj.name}/vendor/glm/glm/**.inl",
-	}
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+        "%{prj.name}/vendor/glm/glm/**.hpp",
+        "%{prj.name}/vendor/glm/glm/**.inl",
+    }
 
-	defines
-	{
-		"_CRT_SECURE_NO_WARNINGS"
-	}
+    defines
+    {
+        "_CRT_SECURE_NO_WARNINGS"
+    }
 
-	includedirs
-	{
-		"%{prj.name}/src",
-		"%{prj.name}/vendor/spdlog/include",
-		"%{IncludeDir.GLFW}",
-		"%{IncludeDir.Glad}",
-		"%{IncludeDir.ImGui}",
-		"%{IncludeDir.glm}"
-	}
+    includedirs
+    {
+        "%{prj.name}/src",
+        "%{prj.name}/vendor/spdlog/include",
+        "%{IncludeDir.GLFW}",
+        "%{IncludeDir.Glad}",
+        "%{IncludeDir.ImGui}",
+        "%{IncludeDir.glm}"
+    }
 
-	links 
-	{ 
-		"GLFW",
-		"Glad",
-		"ImGui",
-		"opengl32.lib"
-	}
+    links 
+    { 
+        "GLFW",
+        "Glad",
+        "ImGui",
+        "opengl32.lib"
+    }
 
-	filter "system:windows"
-		systemversion "latest"
+    filter "system:windows"
+        systemversion "latest" -- Use a different SDK version if possible
 
-		defines
-		{
-			"R2_PLATFORM_WINDOWS",
-			"R2_BUILD_DLL",
-			"GLFW_INCLUDE_NONE"
-		}
+        defines
+        {
+            "R2_PLATFORM_WINDOWS",
+            "GLFW_INCLUDE_NONE"
+        }
 
-		postbuildcommands
-		{
-			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
-		}
+    filter "configurations:Debug"
+        defines "R2_DEBUG"
+        runtime "Debug"
+        symbols "on"
 
-	filter "configurations:Debug"
-		defines { "DEBUG", "IMGUI_API=__declspec(dllexport)" }
-		defines "R2_DEBUG"
-		runtime "Debug"
-		symbols "on"
+    filter "configurations:Release"
+        defines "R2_RELEASE"
+        runtime "Release"
+        optimize "on"
 
-	filter "configurations:Release"
-		defines { "NDEBUG", "IMGUI_API=__declspec(dllexport)" }
-		defines "R2_RELEASE"
-		runtime "Release"
-		optimize "on"
+    filter "configurations:Dist"
+        defines "R2_DIST"
+        runtime "Release"
+        optimize "on"
 
-	filter "configurations:Dist"
-		defines { "NDEBUG", "IMGUI_API=__declspec(dllexport)" }
-		defines "R2_DIST"
-		runtime "Release"
-		optimize "on"
 
 project "Sandbox"
 	location "Sandbox"
@@ -137,19 +153,16 @@ project "Sandbox"
 		}
 
 	filter "configurations:Debug"
-		defines { "DEBUG", "IMGUI_API=__declspec(dllimport)" }
 		defines "R2_DEBUG"
 		runtime "Debug"
 		symbols "on"
 
 	filter "configurations:Release"
-		defines { "NDEBUG", "IMGUI_API=__declspec(dllimport)" }
 		defines "R2_RELEASE"
 		runtime "Release"
 		optimize "on"
 
 	filter "configurations:Dist"
-		defines { "NDEBUG", "IMGUI_API=__declspec(dllimport)" }
 		defines "R2_DIST"
 		runtime "Release"
 		optimize "on"
